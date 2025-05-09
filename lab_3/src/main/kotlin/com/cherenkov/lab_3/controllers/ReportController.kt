a9ce5d8e8171d2fa187606bf5d5fb1a8075d6b6e
AssasinNik <nikitacherenkov228@gmail.com>
2025-05-09 11:25:41 +0300

    Change Route

--- lab_3/src/main/kotlin/com/cherenkov/lab_3/controllers/ReportController.kt
+++ lab_3/src/main/kotlin/com/cherenkov/lab_3/controllers/ReportController.kt
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/reports")
+@RequestMapping("/reports")
 class ReportController(private val reportService: ReportService) {
     private val log = LoggerFactory.getLogger(ReportController::class.java)
 
